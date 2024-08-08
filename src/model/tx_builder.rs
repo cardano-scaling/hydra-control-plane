cc58fb2083562e156a001934b64172639a284bc6
Pi Lanningham <pi@sundaeswap.finance>
2024-08-08 15:43:21 -0400

    Add collateral if we're garbage collecting a player

--- src/model/tx_builder.rs
+++ src/model/tx_builder.rs
@@ -74,6 +74,9 @@ impl TxBuilder {
             .change_address(input_utxo.clone().address)
             .fee(0);
 
+        if expired_utxos.len() > 0 {
+            tx_builder = tx_builder.collateral_input(input_utxo.clone().into());
+        }
         for utxo in expired_utxos {
             tx_builder = tx_builder.input(utxo.clone().into());
             tx_builder = tx_builder.add_spend_redeemer(
