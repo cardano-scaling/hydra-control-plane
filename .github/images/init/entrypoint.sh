883b604168c6c6125ae2059691275da35c8ee435
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-11-25 14:14:58 -0300

    chore: Change how init container handles parameters

--- .github/images/init/entrypoint.sh
+++ .github/images/init/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if aws s3 ls "s3://$BUCKET/$KEY" > /dev/null 2>&1; then
-  echo "Snapshot exists, downloading..."
-  aws s3 cp "s3://$BUCKET/$KEY" "$DATA_DIR"
-  tar -xzvf "$DATA_DIR/$KEY" -C "$DATA_DIR"
-else
+if [[ -z "${URI}" ]]; then
   echo "Snapshot does not exist, generating keys..."
   mkdir "$DATA_DIR/keys"
   /hydra-node gen-hydra-key --output-file "$DATA_DIR/keys/hydra"
+else
+  echo "Downloading snashot..."
+  aws s3 cp "s3://$BUCKET/$KEY" "$DATA_DIR"
+  tar -xzvf "$DATA_DIR/$KEY" -C "$DATA_DIR"
 fi
