e99915df89b04ee8115356023b3b33284644973b
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-11-27 13:46:05 -0300

    chore: Add missing cluster

--- playbook/global/main.tf
+++ playbook/global/main.tf
@@ -34,6 +34,9 @@ module "global" {
   thanos_endpoints = [
     "k8s-hydradoo-thanossi-3e6cc6bace-ddd76e7d5e148d9f.elb.us-east-1.amazonaws.com:10901",
     "k8s-hydradoo-thanossi-08d03cf670-c832566453f2a5a0.elb.eu-central-1.amazonaws.com:10901",
-    "k8s-hydradoo-thanossi-ee478628bf-6cd33f983e73ae8f.elb.us-west-2.amazonaws.com:10901"
+    "k8s-hydradoo-thanossi-ee478628bf-6cd33f983e73ae8f.elb.us-west-2.amazonaws.com:10901",
+    "k8s-hydradoo-thanossi-42bd8b32e4-0818e533e76d43c3.elb.ap-southeast-1.amazonaws.com:10901",
+    "k8s-hydradoo-thanossi-e1183b7f93-f951f656c1c3da07.elb.sa-east-1.amazonaws.com:10901",
+    "k8s-hydradoo-thanossi-13ead426cd-5339600f2c046547.elb.af-south-1.amazonaws.com:10901",
   ]
 }
