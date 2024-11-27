9ad3ab695249f0fdd83e808799d24296c6fd4f90
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-11-27 17:52:52 -0300

    fix: Correctly untar snapshot

--- .github/images/init/entrypoint.sh
+++ .github/images/init/entrypoint.sh
@@ -5,6 +5,6 @@ if [[ -z "${KEY}" ]]; then
   /hydra-node gen-hydra-key --output-file "$DATA_DIR/keys/hydra"
 else
   echo "Downloading snashot..."
-  aws s3 cp "s3://$BUCKET/$KEY" "$DATA_DIR"
-  tar -xzvf "$DATA_DIR/$KEY" -C "$DATA_DIR"
+  aws s3 cp "s3://$BUCKET/$KEY" "$DATA_DIR/snapshot.tar.gz"
+  tar -xzvf "$DATA_DIR/snapshot.tar.gz" -C "$DATA_DIR"
 fi
