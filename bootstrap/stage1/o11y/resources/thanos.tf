ac427a808e2b62041a2d5575de584f62945c31fc
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-12-03 01:51:17 -0300

    fix: Add LB for each shard

--- bootstrap/stage1/o11y/resources/thanos.tf
+++ bootstrap/stage1/o11y/resources/thanos.tf
@@ -12,9 +12,46 @@ resource "kubernetes_service_v1" "thanos_sidecar" {
   spec {
     load_balancer_class = "service.k8s.aws/nlb"
     selector = {
-      "prometheus"                  = "prometheus"
-      "app.kubernetes.io/instance"  = "prometheus"
-      "operator.prometheus.io/name" = "prometheus"
+      "prometheus"                   = "prometheus"
+      "app.kubernetes.io/instance"   = "prometheus"
+      "operator.prometheus.io/name"  = "prometheus"
+      "operator.prometheus.io/shard" = "0"
+    }
+
+    port {
+      name        = "web"
+      port        = 9090
+      target_port = "web"
+    }
+
+    port {
+      name        = "grpc"
+      port        = 10901
+      target_port = 10901
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_service_v1" "thanos_sidecar_1" {
+  metadata {
+    name      = "thanos-sidecar"
+    namespace = var.namespace
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" : "instance"
+      "service.beta.kubernetes.io/aws-load-balancer-scheme" : "internet-facing"
+      "service.beta.kubernetes.io/aws-load-balancer-type" : "external"
+    }
+  }
+
+  spec {
+    load_balancer_class = "service.k8s.aws/nlb"
+    selector = {
+      "prometheus"                   = "prometheus"
+      "app.kubernetes.io/instance"   = "prometheus"
+      "operator.prometheus.io/name"  = "prometheus"
+      "operator.prometheus.io/shard" = "1"
     }
 
     port {
