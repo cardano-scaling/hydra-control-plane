38a91a084476b68122806d207314b6a11a287e3e
yHSJ <jgm0100@mcdaniel.edu>
2024-12-02 23:39:02 -0500

    feat: allow request to specify node id for a new game

--- crates/rpc/src/routes/new_game.rs
+++ crates/rpc/src/routes/new_game.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use anyhow::{anyhow, Context};
-use kube::core::response;
 use rocket::{get, serde::json::Json, State};
 use rocket_errors::anyhow::Result;
 use serde::Serialize;
@@ -64,11 +63,12 @@ pub async fn do_new_game(
     }))
 }
 
-#[get("/new_game?<address>&<player_count>&<bot_count>")]
+#[get("/new_game?<address>&<player_count>&<bot_count>&<node_id>")]
 pub async fn new_game(
     address: &str,
     player_count: Option<u64>,
     bot_count: Option<u64>,
+    node_id: Option<String>,
     state: &State<ClusterState>,
 ) -> Result<Json<NewGameResponse>> {
     info!("Creating a new game for {}", address);
@@ -83,12 +83,19 @@ pub async fn new_game(
     if player_count.is_some_and(|c| bot_count.is_some_and(|b| c + b > 4)) {
         return Result::Err(anyhow!("cannot have more than 4 players and bots").into());
     }
-    let node = state
-        .select_node_for_new_game()
-        .context("error getting warm node")?;
+
+    let node = match node_id {
+        None => state
+            .select_node_for_new_game()
+            .context("error getting warm node")?,
+        Some(node_id) => state.get_node_by_id_for_new_game(node_id)?,
+    };
+
     let node_id = node.metadata.name.clone().context("node without a name")?;
-    do_new_game(node.clone(), address, player_count, bot_count).await.or_else(|e| {
-        state.release_node(node_id.as_str());
-        Err(e)
-    })
+    do_new_game(node.clone(), address, player_count, bot_count)
+        .await
+        .or_else(|e| {
+            state.release_node(node_id.as_str());
+            Err(e)
+        })
 }
