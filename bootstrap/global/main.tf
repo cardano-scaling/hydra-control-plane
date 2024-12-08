a89990004273fd20b6592711b7a9e36f3b61d275
Santiago Carmuega <santiago@carmuega.me>
2024-12-08 08:50:57 -0300

    chore: add discord bot to global

--- bootstrap/global/main.tf
+++ bootstrap/global/main.tf
@@ -19,3 +19,29 @@ variable "external_domain" {
 variable "ssl_cert_arn" {
   type = string
 }
+
+variable "discord_bot_image" {
+  description = "Docker image for the Discord bot"
+  type        = string
+}
+
+variable "discord_bot_token" {
+  description = "Authentication token for the Discord bot"
+  type        = string
+  sensitive   = true
+}
+
+variable "discord_bot_owner_id" {
+  description = "Discord user ID of the bot owner"
+  type        = string
+}
+
+variable "discord_bot_channel_id" {
+  description = "Discord channel ID where the bot will operate"
+  type        = string
+}
+
+variable "discord_bot_admin_role_id" {
+  description = "Discord role ID for bot admins"
+  type        = string
+}
