4d26037a62c39deff415ca02515b1a07bd3c0864
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-12-09 13:30:50 -0300

    chore: Add discord bot lb

--- bootstrap/global/discord-bot.tf
+++ bootstrap/global/discord-bot.tf
@@ -65,3 +65,35 @@ resource "kubernetes_deployment_v1" "discord_bot" {
     }
   }
 }
+
+resource "kubernetes_service_v1" "discord_bot_lb" {
+  metadata {
+    name      = "discord-bot-lb"
+    namespace = var.monitoring_namespace
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" : "instance"
+      "service.beta.kubernetes.io/aws-load-balancer-scheme" : "internet-facing"
+      "service.beta.kubernetes.io/aws-load-balancer-type" : "external"
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-cert" : var.ssl_cert_arn
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-ports" : "443"
+    }
+  }
+
+  spec {
+    type = "LoadBalancer"
+
+    load_balancer_class = "service.k8s.aws/nlb"
+    selector = {
+      "app.kubernetes.io/component" = "o11y"
+      "app.kubernetes.io/name"      = "grafana"
+      "app.kubernetes.io/part-of"   = "hydradoom"
+    }
+
+    port {
+      name        = "http"
+      port        = 443
+      target_port = 8080
+      protocol    = "TCP"
+    }
+  }
+}
